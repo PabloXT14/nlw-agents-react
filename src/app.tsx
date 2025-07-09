65103d439765c19368ae5356504e9d7356ed9b0b
Pablo Alan <pabloxt14@gmail.com>
2025-07-09 18:18:27 -0300

    feat: config shadcn/ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,21 @@
+import { Wand } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello world</h1>
+  return (
+    <main className="flex flex-col items-start gap-4 p-4">
+      <h1>Let me Ask</h1>
+
+      <Button>Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="link">Click me</Button>
+      <Button variant="destructive">Click me</Button>
+
+      <Button size="icon">
+        <Wand />
+      </Button>
+    </main>
+  )
 }
