5f0a4d7df37f1941013d5409deaaefabdd657df4
Pablo Alan <pabloxt14@gmail.com>
2025-07-09 18:53:06 -0300

    feat: add react-query to http requests

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,13 +1,41 @@
+import { useQuery } from '@tanstack/react-query'
+import { Loader2 } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+
+type GetRoomsAPIResponse = {
+  rooms: Array<{
+    id: string
+    name: string
+  }>
+}
 
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: GetRoomsAPIResponse = await response.json()
+
+      const { rooms } = result
+
+      return rooms
+    },
+  })
+
   return (
-    <div>
+    <main className="flex flex-col gap-8 p-4">
       <h1>Create Room</h1>
 
-      <Link className="underline" to="/room/1">
-        Acessar sala
-      </Link>
-    </div>
+      {isLoading && <Loader2 className="animate-spin" />}
+
+      <div className="flex flex-col items-start gap-4">
+        {data?.map((room) => (
+          <Button key={room.id}>
+            <Link to={`/room/${room.id}`}>{room.name}</Link>
+          </Button>
+        ))}
+      </div>
+    </main>
   )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,7 +1,23 @@
+import { Navigate, useParams } from 'react-router-dom'
+
+type RoomParams = {
+  roomId: string
+}
+
 export function Room() {
+  const params = useParams<RoomParams>()
+
+  const { roomId } = params
+
+  if (!roomId) {
+    return <Navigate replace to="/" />
+  }
+
   return (
     <div>
       <h1>Room Details</h1>
+
+      <p>{roomId}</p>
     </div>
   )
 }
