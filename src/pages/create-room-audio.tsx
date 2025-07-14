42be4b69639946efa48b0446ad548657e8931ce7
Pablo Alan <pabloxt14@gmail.com>
2025-07-14 16:55:52 -0300

    refactor: send audio in chunks of 5 seconds each

--- src/pages/create-room-audio.tsx
+++ src/pages/create-room-audio.tsx
@@ -19,21 +19,9 @@ export function CreateRoomAudio() {
   const [isRecording, setIsRecording] = useState(false)
   const recorder = useRef<MediaRecorder | null>(null)
 
-  async function startRecording() {
-    if (!isRecordingSupported) {
-      return alert('Seu navegador nao suporta gravação de audio')
-    }
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
-    setIsRecording(true)
-
-    const audio = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        sampleRate: 44_100,
-      },
-    })
-
+  function createRecorder(audio: MediaStream) {
     recorder.current = new MediaRecorder(audio, {
       mimeType: 'audio/webm', // formato em que vai ser gravado o audio (webm depois sera convertido para mp3)
       audioBitsPerSecond: 64_000,
@@ -56,11 +44,39 @@ export function CreateRoomAudio() {
     recorder.current.start()
   }
 
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      return alert('Seu navegador nao suporta gravação de audio')
+    }
+
+    setIsRecording(true)
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    })
+
+    createRecorder(audio)
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop()
+
+      createRecorder(audio)
+    }, 5000)
+  }
+
   function stopRecording() {
     setIsRecording(false)
 
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop()
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
     }
   }
 
